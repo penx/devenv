98425842a7b8ef10c15da6727ec4c18b3a219bc1
Mark Chambers <mark.chambers1@ibm.com@marks-air.sbank.uk.ibm.com>
2018-02-12 16:42:14 +0000

    removed additional casks

--- casks.sh
+++ casks.sh
@@ -1,33 +1,33 @@
 brew cask install atom
 brew cask install tower
 brew cask install google-chrome
-brew cask install adobe-creative-cloud
-open '/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app'
+# brew cask install adobe-creative-cloud
+# open '/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app'
 brew cask install skype
-brew cask install google-chrome-canary
+# brew cask install google-chrome-canary
 brew cask install firefox
 brew cask install iterm2
-brew cask install dropbox
-brew cask install evernote
+# brew cask install dropbox
+# brew cask install evernote
 brew cask install licecap
-brew cask install java
-brew cask install android-sdk
-brew cask install android-studio
-brew cask install charles
+# brew cask install android-sdk
+# brew cask install android-studio
+# brew cask install charles
 brew cask install docker
 open /Applications/Docker.app
-brew cask install geektool
-brew cask install synergy
-brew cask install dash
-brew cask install postman
+# brew cask install geektool
+# brew cask install synergy
+# brew cask install dash
+# brew cask install postman
 brew cask install sketch
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package
 
-brew cask install font-material-icons
-brew cask install font-materialdesignicons-webfont
-brew cask install font-bebas-neue
-brew cask install font-oswald
-brew cask install font-monoid
+# brew cask install font-material-icons
+# brew cask install font-materialdesignicons-webfont
+# brew cask install font-bebas-neue
+# brew cask install font-oswald
+# brew cask install font-monoid
 
 # Needs password:
-brew cask install virtualbox
+# brew cask install virtualbox
+brew cask install java
