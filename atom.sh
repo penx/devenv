0439baf51a09bec8a8155fd43eafcf7e55058dc7
Mark Chambers <me@marksy.com>
2018-04-06 11:56:33 +0100

    added packages to atom

--- atom.sh
+++ atom.sh
@@ -1,6 +1,6 @@
 # Configure atom sync
 
-apm install sync-settings
+apm install sync-settings api-blueprint-preview change-case css-hyperclick docblockr git-blame git-diff git-plus git-time-machine hyperclick js-hyperclick language-api-blueprint language-babel language-clojure language-css language-docker language-html language-javascript language-json linter linter-eslint linter-ui-default markdown-preview nuclide nuclide-format-js pigments sonarlint sort-lines var-that-color
 
 echo "  \"sync-settings\":
     gistId: \"$GISTID\"
