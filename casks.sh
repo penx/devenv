c00da0ceb31e53e69cdbc264cd7d6209e21b4a49
markips@gmail.com <J8d-uMM-ZMY-whN>
2019-04-01 10:19:08 +1300

    add webstorm, nightowl - reorder and comment

--- casks.sh
+++ casks.sh
@@ -1,17 +1,33 @@
-brew cask install atom
-brew cask install tower
+#!/usr/bin/env bash
+
+## IDEs
+#brew cask install atom
+brew cask install webstorm
+
+## Web browsers
 brew cask install google-chrome
-brew cask install spotify
-brew cask install spotify-notifications
-# brew cask install adobe-creative-cloud
-# open '/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app'
-brew cask install skype
 # brew cask install google-chrome-canary
 brew cask install firefox
-brew cask install iterm2
+
+## Design
+# brew cask install adobe-creative-cloud
+# open '/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app'
+brew cask install sketch
+
+## Media
+brew cask install spotify
+brew cask install spotify-notifications
 brew cask install vlc
-brew cask install scroll-reverser
+
+## Communications
+brew cask install skype
 brew cask install whatsapp
+
+## Utilities
+brew cask install iterm2
+brew cask install slack
+brew cask install scroll-reverser
+brew cask install nightowl
 # brew cask install dropbox
 # brew cask install evernote
 brew cask install licecap
@@ -19,15 +35,16 @@ brew cask install glueprint
 # brew cask install android-sdk
 # brew cask install android-studio
 # brew cask install charles
+#brew cask install tower
 brew cask install docker
 open /Applications/Docker.app
 # brew cask install geektool
 # brew cask install synergy
 # brew cask install dash
 # brew cask install postman
-brew cask install sketch
 brew cask reinstall qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package
 
+## Fonts
 # brew cask install font-material-icons
 # brew cask install font-materialdesignicons-webfont
 # brew cask install font-bebas-neue
