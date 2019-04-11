a246ebeb0adc063b3d76888dcbb40d188634ae71
Alasdair McLeay <alasdair.mcleay@me.com>
2019-04-11 16:13:51 +0100

    node 10, don't install n

--- node.sh
+++ node.sh
@@ -3,10 +3,11 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | zsh
 nvm install 6
 nvm install 8
-nvm use 8
+nvm install 10
+nvm use 10
 
 # n
-npm install -g n
+# npm install -g n
 
 # n 6
 # n 8
