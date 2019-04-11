d9c9aed17b5f7f3ed3872272cc431b8f45f96e24
Alasdair McLeay <alasdair.mcleay@me.com>
2019-04-11 16:13:41 +0100

    tower2, don't install charles

--- casks.sh
+++ casks.sh
@@ -1,6 +1,6 @@
 brew cask install atom
 brew cask install visual-studio-code
-brew cask install tower
+brew cask install tower2
 brew cask install google-chrome
 brew cask install adobe-creative-cloud
 open '/usr/local/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app'
@@ -14,7 +14,7 @@ brew cask install licecap
 brew cask install java
 brew cask install android-sdk
 brew cask install android-studio
-brew cask install charles
+# brew cask install charles
 brew cask install docker
 open /Applications/Docker.app
 brew cask install geektool
@@ -24,7 +24,7 @@ brew cask install postman
 brew cask install sketch
 
 brew cask install istat-menus
-brew cask install intel-power-gadget
 
 # Needs password:
+brew cask install intel-power-gadget
 brew cask install virtualbox
