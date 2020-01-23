030ade63a55c886e412e650c1680e1ac1dc86ace
Alasdair McLeay <penx@users.noreply.github.com>
2020-01-23 15:10:50 +0000

    update nvm v0.35.2

--- node.sh
+++ node.sh
@@ -2,7 +2,7 @@
 
 # nvm
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | zsh
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | zsh
 nvm install 6
 nvm install 8
 nvm install 10
