7551a61487899a404e32fa0baa8e53f116529765
Alasdair McLeay <penx@users.noreply.github.com>
2020-01-23 20:01:21 +0000

    Update node.sh

--- node.sh
+++ node.sh
@@ -3,6 +3,10 @@
 # nvm
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | zsh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install 6
 nvm install 8
 nvm install 10
