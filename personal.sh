e76a028f782aa8e09f81380e5f29de7104578584
Alasdair McLeay <alasdair.mcleay@me.com>
2020-01-23 13:35:54 +0000

    battle net

--- personal.sh
+++ personal.sh
@@ -5,6 +5,7 @@ brew cask install league-of-legends
 brew cask install razer-synapse
 brew cask install steam
 brew cask install twitch
+brew cask install battle-net
 
 #hue sync
 brew cask install philips-hue-sync
