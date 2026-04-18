2316107123b1065dddc3b86f20691c09ccab18bd
Alasdair McLeay <alasdair.mcleay@rvu.co.uk>
2026-04-18 20:27:59 +0100

    Make Homebrew path portable across architectures in 1-init.sh

--- 1-init.sh
+++ 1-init.sh
@@ -11,9 +11,17 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 touch ~/.profile
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+if [ "$(uname -m)" = "arm64" ]; then
+  BREW_PATH=/opt/homebrew/bin/brew
+elif [ "$(uname -m)" = "x86_64" ]; then
+  BREW_PATH=/usr/local/bin/brew
+else
+  BREW_PATH=$(which brew)
+fi
+
 echo >>~/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+echo "eval \"\$(${BREW_PATH} shellenv)\"" >>~/.zprofile
+eval "$(${BREW_PATH} shellenv)"
 
 echo 'export EDITOR=code' >>~/.profile
 
